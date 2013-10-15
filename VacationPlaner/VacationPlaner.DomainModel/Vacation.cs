a18479ca3a28652ca4c7901fbf9eb448d5934b63
Tibi <tibi@covaci.se>
2013-10-15 11:34:04 +0300

    Domain model added

---
+++ VacationPlaner/VacationPlaner.DomainModel/Vacation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacationPlaner.DomainModel
+{
+    public class Vacation
+    {
+        public Vacation()
+        {
+            Persons = new HashSet<Person>();
+        }
+
+        public int Id { get; set; }
+        public string VacationName { get; set; }
+        public Address Address { get; set; }
+
+        public ICollection<Person> Persons { get; set; }
+
+        public void AddParticipantToTheParty(Person person)
+        {
+            Persons.Add(person);
+        }
+    }
+}
